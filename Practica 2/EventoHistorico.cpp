1bc0137e8b5fb38a5f6d7dbeaec082a626c3c11e
fjsaezm <fjaviersaezm@gmail.com>
2016-10-27 00:07:46 +0200

    añado algo

---
+++ Practica 2/EventoHistorico.cpp
@@ -0,0 +1,18 @@
+
+EventoHistorico::EventoHistorico(int n, fstream f){
+
+  fecha = n;
+
+
+
+}
+
+string EventoHistorico::evento(int n){
+
+  return eventos[i];
+
+}
+
+void addEvento(const string& nombre){
+
+  eventos.push
--- Practica 2/EventoHistorico.h
+++ Practica 2/EventoHistorico.h
@@ -1,20 +1,37 @@
-#ifndef __EVENTO_H__
-#define __EVENTO_H__
+#ifndef EVENTO_H
+#define EVENTO_H
 
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include "EventoHistorico.cpp"
+
+
 
 class EventoHistorico {
+
+/**
+ * @page repConjunto Rep del TDA Evento Histórico
+ * @section invConjunto Invariante de la representación
+ *
+ * El invariante de este conjunto es el orden de los elementos del vector de strings que contiene los nombres de los eventos, pues el orden es irrelevante. 
+ * 
+ * @section faConjunto Función de abstracción
+ * 
+ * Un objeto válido @e rep del TDA Evento Histórico representa a una fecha con un conjunto de eventos que han ocurrido en esa fecha
+ *
+ */
 
       int fecha;
       vector<string> eventos;
 
       public:
 
+
       EventoHistorico():fecha(0){}
-      EventoHistorico(int n,wñkdhgpdhfsagh);
-      ~EventoHistorico();
+      EventoHistorico(int n,fstream f);
+      ~EventoHistorico() = default();
 
       int Fecha(){return fecha;}
       string evento(int n){return eventos[i];}
@@ -26,4 +43,5 @@ class EventoHistorico {
 
 friend ostream operator << (ostream& os, EventoHistorico evento);
 friend istream operator >> (const istream& is, EventoHistorico evento);
-#endif
+
+#endif // EVENTO_H
